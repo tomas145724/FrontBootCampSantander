3b7ac3c1315179f90f31437270c20d3793d74a95
tomas145724 <tomas145724@gmail.com>
2021-06-04 01:31:35 -0300

    Bootcamp Frontend

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { FormsModule } from '@angular/forms';
+import { StockCardComponent } from './stock-card/stock-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardPageComponent,
+    StockCardComponent,
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+
+  ],
+  exports: [
+    DashboardPageComponent,
+  ],
+
+})
+
+export class DashboardModule { }
---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import Stock from '../shared/models/stock-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+
+  readonly baseUrl = 'https://bootcamp-dev-week.herokuapp.com';
+
+  constructor(private http: HttpClient) { }
+
+  async getStocks(): Promise<Stock[]> {
+    return this.http.get<Stock[]>(`${this.baseUrl}/stock`).toPromise();
+  }
+
+}
